f1889d809085b742fe9cd8de709165a9c80af8ff
Alunderis <arturas.alunderis@gmail.com>
2023-08-09 18:48:55 +0300

    Initial commit

---
+++ src/test/java/skelbiu.java
@@ -0,0 +1,15 @@
+import org.testng.annotations.Test;
+
+public class skelbiu {
+
+    @Test
+    public void init(){
+        System.out.println("hi");
+    }
+@Test
+    public void openWebsite () {
+
+
+}
+
+}
